717488a9149221f83821d535607bba085e0a4aba
amelie-certin <amelie.certin@gmail.com>
2020-01-26 17:33:26 +0100

    Fix electionList

--- client/src/components/ElectionList.js
+++ client/src/components/ElectionList.js
@@ -1,39 +1,44 @@
 import React from "react";
 import ElectionCard from "./ElectionCard";
+import drizzle from "../store";
 
 class ElectionList extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("list", props);
-    this.state = { dataKey: null };
-  }
+    constructor(props) {
+        super(props);
+        this.state = { dataKey: null };
+    }
 
-  componentDidMount() {
-    // const { drizzle, drizzleState } = this.props;
-    // const contract = this.props.contract;
-    //
-    // if (this.props.status.initialized) {
-    //   const dataKey = contract.methods.listElections.cacheCall();
-    //   console.log(dataKey);
-    //   this.setState({ dataKey });
-    // }
-  }
+    componentDidMount() {
+        if (this.props.status.initialized) {
+            const Vote = drizzle.contracts.Vote;
+            const dataKey = Vote.methods.listElections.cacheCall();
+            this.setState({ dataKey });
+        }
+    }
 
-  render() {
-    // const Vote  = this.props.contract;
+    render() {
+        const Vote = drizzle.contracts.Vote;
+        const electionList = Vote.methods.listElections[this.state.dataKey];
 
-    // let electionList = Vote.listElections[this.state.dataKey];
-    // if (electionList && false) {
-    //   return(
-    //     <div>
-    //       {electionList.value.map((election, i) => {
-    //         return (<ElectionCard key={i} election={election} />)
-    //       })}
-    //     </div>
-    //   );
-    // }
-    return "Elections";
-  }
+        return(
+            <div>
+                <h1>Toutes les élections</h1>
+                {this._renderElectionList(electionList)}
+            </div>
+        );
+    }
+
+    _renderElectionList(electionList) {
+        if (electionList) {
+            return(
+                <div>
+                    {electionList.value.map((election, i) => {
+                        return (<ElectionCard key={i} election={election} />)
+                    })}
+                </div>
+            );
+        }
+    }
 }
 
 export default ElectionList;
